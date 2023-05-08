fa7715531f76d2853ad496ba2c1aab268f062be1
admin <admin@YD02-04>
2023-05-08 15:27:34 +0900

    first commit

---
+++ chap01/src/com/yedam/chap01/Example01.java
@@ -0,0 +1,14 @@
+package com.yedam.chap01;  //지금 어디 패키지?
+
+public class Example01 {  //지금 사용하고 있는 파일명
+
+	public static void main(String[] args) {  //메소드 선언. Java는 main메소드에서 실행시작(무조건 있어야 실행가능)
+		System.out.println("Hello World");  //자바스크립트랑 가장 큰 차이점 -> 실행문이 끝나면 반드시 세미콜론
+		//주석 -> 한줄 주석
+		/*
+		 *    -> 여러줄 주석
+		 */
+		//주석은 실행시 .class파일에 포함되지 않음 = 실행시 주석을 뺌
+		
+	}
+}
